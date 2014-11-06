9640ba6e2f05b035f04aa4b491d6ad4333078516
rodrigoror <rodrigoror@gmail.com>
2014-11-06 01:00:15 +0000

    alteração das estruturas de pastas e modificações nos menus

--- teste02/src/main/java/org/rlabs/teste02/business/MenuBC.java
+++ teste02/src/main/java/org/rlabs/teste02/business/MenuBC.java
@@ -1,7 +1,9 @@
 package org.rlabs.teste02.business;
 
+import br.gov.frameworkdemoiselle.lifecycle.Startup;
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
 import br.gov.frameworkdemoiselle.template.DelegateCrud;
+import br.gov.frameworkdemoiselle.transaction.Transactional;
 
 import org.rlabs.teste02.domain.Menu;
 import org.rlabs.teste02.persistence.MenuDAO;
@@ -10,5 +12,21 @@ import org.rlabs.teste02.persistence.MenuDAO;
 public class MenuBC extends DelegateCrud<Menu, Long, MenuDAO> {
 	
 	private static final long serialVersionUID = 1L;
-	
+	@Startup
+	@Transactional
+	public void load() {
+		if (findAll().isEmpty()) {
+			//insert(new Menu("Classe", "Link","Permissao","Nome","Parent"));
+			insert(new Menu("fa fa-dashboard fa-fw", "index.jsf","ALL","Dashboard","_parent"));
+			insert(new Menu("fa fa-bar-chart-o fa-fw", "index.jsf","ALL","Charts","_parent"));
+			insert(new Menu("fa fa-table fa-fw", "index.jsf","ALL","Tables","_parent"));
+			insert(new Menu("fa fa-edit fa-fw", "index.jsf","ALL","Forms","_parent"));
+			insert(new Menu("fa fa-wrench fa-fw", "index.jsf","ALL","UI Elements","_parent"));
+			insert(new Menu("fa fa-sitemap fa-fw", "index.jsf","ALL","Multi-Level","_parent"));
+			insert(new Menu("fa fa-files-o fa-fw", "index.jsf","ALL"," Sample Pages","_parent"));
+			insert(new Menu("fa fa-table fa-fw", "bookmark_list.jsf","ALL","Bookmarks","_parent"));
+			insert(new Menu("fa fa-table fa-fw", "menu_list.jsf","ALL","Menus","_parent"));
+
+		}
+	}
 }
