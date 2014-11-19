c45ffe07db295e3c6c2da8f585baafbc7f4fcad5
rodrigoror <rodrigoror@gmail.com>
2014-11-19 02:12:37 +0000

    criação das classes iniciais

---
+++ teste02/src/main/java/org/rlabs/teste02/domain/CompanyEnd.java
@@ -0,0 +1,323 @@
+/**
+ * 
+ */
+package org.rlabs.teste02.domain;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * @author rodrigo@celebrandoavida.com
+ */
+@Entity
+@Table(name="COMPANYEND"/*, schema="SCHEMA"*/)
+public class CompanyEnd implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * @uml.property  name="comp_id"
+	 */
+	@Id
+	@GeneratedValue(strategy = SEQUENCE)
+	@Column(name="cend_id", nullable=false)
+	private Long cend_id;
+	
+	@Column(name="cend_cep",nullable=false)
+	@Length(max=9)
+	@NotNull
+	private String cend_cep;
+	
+	@Column(name="cend_end")
+	private String cend_end;
+	
+	@Column(name="cend_num")
+	private String cend_num;
+	
+	@Column(name="cend_comp")
+	private String cend_comp;
+	
+	@Column(name="cend_bairro")
+	private String cend_bairro;
+	
+	@Column(name="cend_cidade")
+	private String cend_cidade;
+	
+	@Column(name="cend_estado")
+	private String cend_estado;
+	
+	@Column(name="cend_ativo")
+	private Boolean cend_ativo;
+	
+	@Column(name="cend_excluido")
+	private Boolean cend_excluido;
+
+	@Column(name="cend_ipUpdate", nullable=false)
+	@NotNull
+	private String cend_ipUpdate;
+	
+	@Column(name="cend_loginUpdate", nullable=false)
+	@NotNull
+	private String cend_loginUpdate;
+	
+	@Column(name="cend_dtUpdate", nullable=false)
+	@NotNull
+	@Temporal(value = TemporalType.TIMESTAMP)
+	private Date cend_dtUpdate;
+	
+	@ManyToOne
+    @JoinColumn(name="comp_id", nullable=false)	
+	private Company cend_company;
+
+	public CompanyEnd() {
+		super();
+	}
+
+	/**
+	 * @param cend_cep
+	 * @param cend_end
+	 * @param cend_num
+	 * @param cend_comp
+	 * @param cend_bairro
+	 * @param cend_cidade
+	 * @param cend_estado
+	 * @param cend_ativo
+	 * @param cend_excluido
+	 * @param cend_ipUpdate
+	 * @param cend_loginUpdate
+	 * @param cend_dtUpdate
+	 * @param cend_company
+	 */
+	public CompanyEnd(String cend_cep, String cend_end, String cend_num,
+			String cend_comp, String cend_bairro, String cend_cidade,
+			String cend_estado, Boolean cend_ativo, Boolean cend_excluido,
+			String cend_ipUpdate, String cend_loginUpdate, Date cend_dtUpdate,
+			Company cend_company) {
+		super();
+		this.cend_cep = cend_cep;
+		this.cend_end = cend_end;
+		this.cend_num = cend_num;
+		this.cend_comp = cend_comp;
+		this.cend_bairro = cend_bairro;
+		this.cend_cidade = cend_cidade;
+		this.cend_estado = cend_estado;
+		this.cend_ativo = cend_ativo;
+		this.cend_excluido = cend_excluido;
+		this.cend_ipUpdate = cend_ipUpdate;
+		this.cend_loginUpdate = cend_loginUpdate;
+		this.cend_dtUpdate = cend_dtUpdate;
+		this.cend_company = cend_company;
+	}
+
+	/**
+	 * @return the cend_id
+	 */
+	public Long getCend_id() {
+		return cend_id;
+	}
+
+	/**
+	 * @param cend_id the cend_id to set
+	 */
+	public void setCend_id(Long cend_id) {
+		this.cend_id = cend_id;
+	}
+
+	/**
+	 * @return the cend_cep
+	 */
+	public String getCend_cep() {
+		return cend_cep;
+	}
+
+	/**
+	 * @param cend_cep the cend_cep to set
+	 */
+	public void setCend_cep(String cend_cep) {
+		this.cend_cep = cend_cep;
+	}
+
+	/**
+	 * @return the cend_end
+	 */
+	public String getCend_end() {
+		return cend_end;
+	}
+
+	/**
+	 * @param cend_end the cend_end to set
+	 */
+	public void setCend_end(String cend_end) {
+		this.cend_end = cend_end;
+	}
+
+	/**
+	 * @return the cend_num
+	 */
+	public String getCend_num() {
+		return cend_num;
+	}
+
+	/**
+	 * @param cend_num the cend_num to set
+	 */
+	public void setCend_num(String cend_num) {
+		this.cend_num = cend_num;
+	}
+
+	/**
+	 * @return the cend_comp
+	 */
+	public String getCend_comp() {
+		return cend_comp;
+	}
+
+	/**
+	 * @param cend_comp the cend_comp to set
+	 */
+	public void setCend_comp(String cend_comp) {
+		this.cend_comp = cend_comp;
+	}
+
+	/**
+	 * @return the cend_bairro
+	 */
+	public String getCend_bairro() {
+		return cend_bairro;
+	}
+
+	/**
+	 * @param cend_bairro the cend_bairro to set
+	 */
+	public void setCend_bairro(String cend_bairro) {
+		this.cend_bairro = cend_bairro;
+	}
+
+	/**
+	 * @return the cend_cidade
+	 */
+	public String getCend_cidade() {
+		return cend_cidade;
+	}
+
+	/**
+	 * @param cend_cidade the cend_cidade to set
+	 */
+	public void setCend_cidade(String cend_cidade) {
+		this.cend_cidade = cend_cidade;
+	}
+
+	/**
+	 * @return the cend_estado
+	 */
+	public String getCend_estado() {
+		return cend_estado;
+	}
+
+	/**
+	 * @param cend_estado the cend_estado to set
+	 */
+	public void setCend_estado(String cend_estado) {
+		this.cend_estado = cend_estado;
+	}
+
+	/**
+	 * @return the cend_ativo
+	 */
+	public Boolean getCend_ativo() {
+		return cend_ativo;
+	}
+
+	/**
+	 * @param cend_ativo the cend_ativo to set
+	 */
+	public void setCend_ativo(Boolean cend_ativo) {
+		this.cend_ativo = cend_ativo;
+	}
+
+	/**
+	 * @return the cend_excluido
+	 */
+	public Boolean getCend_excluido() {
+		return cend_excluido;
+	}
+
+	/**
+	 * @param cend_excluido the cend_excluido to set
+	 */
+	public void setCend_excluido(Boolean cend_excluido) {
+		this.cend_excluido = cend_excluido;
+	}
+
+	/**
+	 * @return the cend_ipUpdate
+	 */
+	public String getCend_ipUpdate() {
+		return cend_ipUpdate;
+	}
+
+	/**
+	 * @param cend_ipUpdate the cend_ipUpdate to set
+	 */
+	public void setCend_ipUpdate(String cend_ipUpdate) {
+		this.cend_ipUpdate = cend_ipUpdate;
+	}
+
+	/**
+	 * @return the cend_loginUpdate
+	 */
+	public String getCend_loginUpdate() {
+		return cend_loginUpdate;
+	}
+
+	/**
+	 * @param cend_loginUpdate the cend_loginUpdate to set
+	 */
+	public void setCend_loginUpdate(String cend_loginUpdate) {
+		this.cend_loginUpdate = cend_loginUpdate;
+	}
+
+	/**
+	 * @return the cend_dtUpdate
+	 */
+	public Date getCend_dtUpdate() {
+		return cend_dtUpdate;
+	}
+
+	/**
+	 * @param cend_dtUpdate the cend_dtUpdate to set
+	 */
+	public void setCend_dtUpdate(Date cend_dtUpdate) {
+		this.cend_dtUpdate = cend_dtUpdate;
+	}
+
+	/**
+	 * @return the cend_company
+	 */
+	public Company getCend_company() {
+		return cend_company;
+	}
+
+	/**
+	 * @param cend_company the cend_company to set
+	 */
+	public void setCend_company(Company cend_company) {
+		this.cend_company = cend_company;
+	}
+	
+}
--- teste02/src/main/java/org/rlabs/teste02/domain/Users.java
+++ teste02/src/main/java/org/rlabs/teste02/domain/Users.java
@@ -9,18 +9,26 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
 /**
  * @author rodrigo.ramos
  *
  */
+@Entity
+@Table(name="USERS"/*, schema="SCHEMA"*/)
 public class Users implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -39,88 +47,297 @@ public class Users implements Serializable{
 	@Column(name="user_login", length=50, nullable=false, unique=true)
 	@NotBlank
 	@Email
+	@Length(max=50)
 	private String user_login;
 	
 	/**
 	 * Para uma futura integracao
 	 */
 	@Column(name="user_cpf", length=11, nullable=false)
-	//@Cpf
 	@NotBlank
+	@Length(max=11)
 	private String user_cpf;
 	
-	/**
-	 */
 	@Column(name="user_senha", nullable=false, length=100)
-	@NotBlank
+	@NotNull
+	@Length(max=100)
 	private String user_senha;
 	
-	/**
-	 * Ativo
-	 */
+	@Column(name="user_susep", length=11, unique=true)
+	@NotNull
+	@Length(max=11)
+	private String user_susep;
+
+	@Column(name="user_nome", nullable=false, length=100)
+	@NotNull
+	@Length(max=100)
+	private String user_nome;
+	
+
+	@Column(name="user_telefone", length=10)
+	@Length(max=10)
+	private String user_telefone;
+	
 	@Column(name="user_ativo")
 	private Boolean user_ativo;
 	
-	/**
-	 */
+	@Column(name="user_excluido")
+	private Boolean user_excluido;
+
+	@Column(name="user_ipUpdate", nullable=false)
+	private String user_ipUpdate;
+	
+	@Column(name="user_loginUpdate", nullable=false)
+	private String user_loginUpdate;
+	
+	@Column(name="user_dtUpdate", nullable=false)
+	@Temporal(value = TemporalType.TIMESTAMP)
+	private Date user_dtUpdate;
+	
 	@Column(name="user_perfil", nullable=false)
-	@NotBlank
+	@NotNull
 	private String user_perfil;
 	
-	/**
-	 * 
-	 */
-	@Column(name="user_insertUpdate", length=50, nullable=false)
-	private String user_insertUpdate;
+	@ManyToOne
+    @JoinColumn(name="comp_id", nullable=false)	
+	private Company user_company;
 	
+	public Users() {
+		super();
+	}
+
 	/**
-	 * 
+	 * @param user_login
+	 * @param user_cpf
+	 * @param user_senha
+	 * @param user_susep
+	 * @param user_nome
+	 * @param user_telefone
+	 * @param user_ativo
+	 * @param user_excluido
+	 * @param user_ipUpdate
+	 * @param user_loginUpdate
+	 * @param user_dtUpdate
+	 * @param user_perfil
+	 * @param user_company
 	 */
-	@Column(name="user_dtInsertUpdate")
-	@Temporal(value=TemporalType.DATE)
-	private Date user_dtInsertUpdate;
+	public Users(String user_login, String user_cpf, String user_senha,
+			String user_susep, String user_nome, String user_telefone,
+			Boolean user_ativo, Boolean user_excluido, String user_ipUpdate,
+			String user_loginUpdate, Date user_dtUpdate, String user_perfil,
+			Company user_company) {
+		super();
+		this.user_login = user_login;
+		this.user_cpf = user_cpf;
+		this.user_senha = user_senha;
+		this.user_susep = user_susep;
+		this.user_nome = user_nome;
+		this.user_telefone = user_telefone;
+		this.user_ativo = user_ativo;
+		this.user_excluido = user_excluido;
+		this.user_ipUpdate = user_ipUpdate;
+		this.user_loginUpdate = user_loginUpdate;
+		this.user_dtUpdate = user_dtUpdate;
+		this.user_perfil = user_perfil;
+		this.user_company = user_company;
+	}
+
+	/**
+	 * @return the user_id
+	 */
+	public Long getUser_id() {
+		return user_id;
+	}
+
+	/**
+	 * @param user_id the user_id to set
+	 */
+	public void setUser_id(Long user_id) {
+		this.user_id = user_id;
+	}
+
+	/**
+	 * @return the user_login
+	 */
+	public String getUser_login() {
+		return user_login;
+	}
+
+	/**
+	 * @param user_login the user_login to set
+	 */
+	public void setUser_login(String user_login) {
+		this.user_login = user_login;
+	}
+
+	/**
+	 * @return the user_cpf
+	 */
+	public String getUser_cpf() {
+		return user_cpf;
+	}
+
+	/**
+	 * @param user_cpf the user_cpf to set
+	 */
+	public void setUser_cpf(String user_cpf) {
+		this.user_cpf = user_cpf;
+	}
+
+	/**
+	 * @return the user_senha
+	 */
+	public String getUser_senha() {
+		return user_senha;
+	}
+
+	/**
+	 * @param user_senha the user_senha to set
+	 */
+	public void setUser_senha(String user_senha) {
+		this.user_senha = user_senha;
+	}
+
+	/**
+	 * @return the user_susep
+	 */
+	public String getUser_susep() {
+		return user_susep;
+	}
+
+	/**
+	 * @param user_susep the user_susep to set
+	 */
+	public void setUser_susep(String user_susep) {
+		this.user_susep = user_susep;
+	}
+
+	/**
+	 * @return the user_nome
+	 */
+	public String getUser_nome() {
+		return user_nome;
+	}
+
+	/**
+	 * @param user_nome the user_nome to set
+	 */
+	public void setUser_nome(String user_nome) {
+		this.user_nome = user_nome;
+	}
+
+	/**
+	 * @return the user_telefone
+	 */
+	public String getUser_telefone() {
+		return user_telefone;
+	}
+
+	/**
+	 * @param user_telefone the user_telefone to set
+	 */
+	public void setUser_telefone(String user_telefone) {
+		this.user_telefone = user_telefone;
+	}
+
+	/**
+	 * @return the user_ativo
+	 */
+	public Boolean getUser_ativo() {
+		return user_ativo;
+	}
+
+	/**
+	 * @param user_ativo the user_ativo to set
+	 */
+	public void setUser_ativo(Boolean user_ativo) {
+		this.user_ativo = user_ativo;
+	}
+
+	/**
+	 * @return the user_excluido
+	 */
+	public Boolean getUser_excluido() {
+		return user_excluido;
+	}
+
+	/**
+	 * @param user_excluido the user_excluido to set
+	 */
+	public void setUser_excluido(Boolean user_excluido) {
+		this.user_excluido = user_excluido;
+	}
+
+	/**
+	 * @return the user_ipUpdate
+	 */
+	public String getUser_ipUpdate() {
+		return user_ipUpdate;
+	}
+
+	/**
+	 * @param user_ipUpdate the user_ipUpdate to set
+	 */
+	public void setUser_ipUpdate(String user_ipUpdate) {
+		this.user_ipUpdate = user_ipUpdate;
+	}
+
+	/**
+	 * @return the user_loginUpdate
+	 */
+	public String getUser_loginUpdate() {
+		return user_loginUpdate;
+	}
+
+	/**
+	 * @param user_loginUpdate the user_loginUpdate to set
+	 */
+	public void setUser_loginUpdate(String user_loginUpdate) {
+		this.user_loginUpdate = user_loginUpdate;
+	}
+
+	/**
+	 * @return the user_dtUpdate
+	 */
+	public Date getUser_dtUpdate() {
+		return user_dtUpdate;
+	}
+
+	/**
+	 * @param user_dtUpdate the user_dtUpdate to set
+	 */
+	public void setUser_dtUpdate(Date user_dtUpdate) {
+		this.user_dtUpdate = user_dtUpdate;
+	}
+
+	/**
+	 * @return the user_perfil
+	 */
+	public String getUser_perfil() {
+		return user_perfil;
+	}
+
+	/**
+	 * @param user_perfil the user_perfil to set
+	 */
+	public void setUser_perfil(String user_perfil) {
+		this.user_perfil = user_perfil;
+	}
+
+	/**
+	 * @return the user_company
+	 */
+	public Company getUser_company() {
+		return user_company;
+	}
+
+	/**
+	 * @param user_company the user_company to set
+	 */
+	public void setUser_company(Company user_company) {
+		this.user_company = user_company;
+	}
 	
-	/**
-	 * 
-	 */
-	@Column(name="user_susep", length=11)
-	private String user_susep;
-	
-	/**
-	 * 
-	 */
-	@Column(name="user_nome", nullable=false, length=50)
-	@NotBlank
-	private String user_nome;
-	
-	/**
-	 * 
-	 */
-	@Column(name="user_telefone", length=10)
-	private String user_telefone;
-	
-	/**
-	 * 
-	 */
-	@Column(name="user_celphone", length=11)
-	private String user_celphone;
-	
-	/**
-	 * 
-	 */
-	@Column(name="user_endereco", length=250)
-	private String user_endereco;
-	
-	/**
-	 * 
-	 */
-	@Column(name="user_cep", length=12)
-	private String user_cep;
-	
-	/**
-	 * 
-	 */
-	@Column(name="user_emailSecundario", length=120)
-	private String user_emailSecundario;
+	//TODO: INSERIR O PERFIL
 	
 }
