70c92471e6fa17490c9756fae290c959898a2769
rodrigoror <rodrigoror@gmail.com>
2014-11-21 01:38:08 +0000

    corrigindo as senhas

--- teste02/src/main/java/org/rlabs/teste02/util/SenhaUtil.java
+++ teste02/src/main/java/org/rlabs/teste02/util/SenhaUtil.java
@@ -23,4 +23,38 @@ public class SenhaUtil implements Serializable{
         
         return sen;  
     }
+	
+	public static String sha256(String senha){
+		//String senha = "123456";
+		 
+        MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        md.update(senha.getBytes());
+ 
+        byte byteData[] = md.digest();
+ 
+        //convert the byte to hex format method 1
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+         sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        System.out.println("Hex format : " + sb.toString());
+ 
+        //convert the byte to hex format method 2
+        StringBuffer hexString = new StringBuffer();
+    	for (int i=0;i<byteData.length;i++) {
+    		String hex=Integer.toHexString(0xff & byteData[i]);
+   	     	if(hex.length()==1) hexString.append('0');
+   	     	hexString.append(hex);
+    	}
+    	System.out.println("Hex format : " + hexString.toString());
+		
+		return senha;
+		
+	}
 }
