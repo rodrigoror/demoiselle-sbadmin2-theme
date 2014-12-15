c53ec52c2cc7c16ad3db1a0fc929a4dd6d397261
rodrigoror <rodrigoror@gmail.com>
2014-12-15 19:04:10 +0000

    Sistema em funcionamento, porém com erros pontuais.

--- teste02/src/main/java/org/rlabs/teste02/persistence/SubMenuDAO.java
+++ teste02/src/main/java/org/rlabs/teste02/persistence/SubMenuDAO.java
@@ -1,5 +1,10 @@
 package org.rlabs.teste02.persistence;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
 import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
 import br.gov.frameworkdemoiselle.template.JPACrud;
 
@@ -10,8 +15,25 @@ public class SubMenuDAO extends JPACrud<SubMenu, Long> {
 
 	private static final long serialVersionUID = 1L;
 
+	@Inject
+	private EntityManager em;
+	
 	public Boolean getActiveList() {
 		return true;
+	}
+
+	public List<SubMenu> getSubMenu(Long menu_id) {
+		String qr = "SELECT sm "
+				+ "FROM SubMenu sm "
+				+ "WHERE sm.smen_menu.menu_id = :pId";
+		
+		@SuppressWarnings("unchecked")
+		List<SubMenu> retorno = em.createQuery(qr)
+				.setParameter("pId", menu_id)
+				.getResultList();
+		
+		
+		return retorno;
 	}
 	
 
--- teste02/src/main/java/org/rlabs/teste02/view/MenuEditMB.java
+++ teste02/src/main/java/org/rlabs/teste02/view/MenuEditMB.java
@@ -1,7 +1,6 @@
 package org.rlabs.teste02.view;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 import br.gov.frameworkdemoiselle.annotation.PreviousView;
@@ -10,6 +9,7 @@ import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
 import br.gov.frameworkdemoiselle.transaction.Transactional;
 import org.rlabs.teste02.business.MenuBC;
 import org.rlabs.teste02.domain.Menu;
+import org.rlabs.teste02.util.IconsUtil;
 
 @ViewController
 @PreviousView("./menu_list.jsf")
@@ -47,18 +47,12 @@ public class MenuEditMB extends AbstractEditPageBean<Menu, Long> {
 		return this.menuBC.load(id);
 	}
 	
-	public List<String> getClasses(){
-		List<String> retorno = new ArrayList<String>();
-		
-		retorno.add("fa fa-dashboard fa-fw");
-		retorno.add("fa fa-bar-chart-o fa-fw");
-		retorno.add("fa fa-table fa-fw");
-		retorno.add("fa fa-edit fa-fw");
-		retorno.add("fa fa-wrench fa-fw");
-		retorno.add("fa fa-sitemap fa-fw");
-		retorno.add("fa fa-files-o fa-fw");
-		
-		return retorno;
+	/**
+	 * Pegando as classes
+	 * @return
+	 */
+	public Map<String,String> getClasses(){
+		return IconsUtil.getWebApplicationIcons();
 	}
 
 }
